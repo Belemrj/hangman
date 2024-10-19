40cbff050a59e48853ed87f6dcb2fdad76405a11
Ana Belem Romero Jiménez <ada_hack579@womenintech.mx>
2024-10-19 08:51:47 -0600

    Dinámica y lógica para adivinar las letras de la palabra

--- main.py
+++ main.py
@@ -17,8 +17,51 @@ def give_instructions(word):
     letters = str(len(word))
     print("Tu palabra tiene " + letters +" letras")
 
+"""Ejemplo de ciclo
+x = 1
+while x <=  5:
+    print(x)
+    x = x + 1  
+    """  
+
+#Función que dibuje las líneas en forma horizontal, de acuerdo al número de letras contenidas en la palabra
+def lines(word):
+    final_string = ""
+    letters = len(word)
+    while letters > 0:
+        final_string += " __ "
+        letters -= 1
+    print (final_string)
+
+#Fnción para adivinar las letras
+def guess(letter,word,guesses):
+    print(letter)
+    x = 0 
+    final_string = ""
+    while x < len(word):
+        if word[x] == letter:
+            final_string += letter
+        elif word[x] in guesses:
+            final_string += word[x]
+        else: 
+            final_string += " __ "
+        x += 1
+
+
+    print(final_string)    
+
+
+
 def play():
     play_word = get_random_word(palabras)
     give_instructions(play_word)
+    lines(play_word)
+    chances = 10
+    guesses = ""
+    while chances > 0:
+        response = input("Elige una letra: ")
+        guess(response, play_word, guesses)
+        guesses += response
+        chances -= 1
 
 play()
